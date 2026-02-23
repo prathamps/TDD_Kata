2b8869985b37a8868f0bc41d4afb514ba3ba3115
prathamps <pspratham16@gmail.com>
2026-02-23 18:03:01 +0530

    refactor: refactored the isLeapYear function

--- leap-year/src/leapYear.ts
+++ leap-year/src/leapYear.ts
@@ -1,6 +1,4 @@
 export const isLeapYear = (year: number) => {
-    if(year%400 === 0) return true
-    if(year%100 === 0) return false
-    if(year%4 === 0) return true
+    if(year%400 === 0 || (year % 4 === 0 && year % 100 !== 0) ) return true
     return false
 }
