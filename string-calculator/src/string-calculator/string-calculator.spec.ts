9e57c89aa7131dcba8ccebf1b7acf7ba2638f086
prathamps <pspratham16@gmail.com>
2026-02-26 12:32:40 +0530

    test: should return number itself for a string containing single number

--- string-calculator/src/string-calculator/string-calculator.spec.ts
+++ string-calculator/src/string-calculator/string-calculator.spec.ts
@@ -6,5 +6,12 @@ describe("StringCalculator", () => {
         expect(add("")).toBe(0);
     })
 
+    it("should return number itself for string containing single number", ()=>{
+        expect(add('1')).toBe(1)
+        expect(add('2')).toBe(2)
+        expect(add('3')).toBe(3)
+        expect(add('21')).toBe(21)
+
+    })
 
 })
--- string-calculator/src/string-calculator/string-calculator.ts
+++ string-calculator/src/string-calculator/string-calculator.ts
@@ -1,3 +1,5 @@
 export const add = (input_string: string) => {
+    if(input_string.length > 0) return Number.parseInt(input_string);
+
     return 0;
 }
