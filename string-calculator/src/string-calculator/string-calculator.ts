71dbb12dc38b75a2a9185112ed5dde9ae55b3084
prathamps <pspratham16@gmail.com>
2026-02-26 12:16:57 +0530

    setup: setup for TDD string-calculator-kata

--- string-calculator/src/string-calculator/string-calculator.ts
+++
@@ -1,7 +0,0 @@
-
-
-// if(input.length>0) {
-//     const num_parts: string[] = inputString.split(",")
-//     if(num_parts.length===1) return parseInt(num_parts[0])
-//     return parseInt(num_parts[0]) + parseInt(num_parts[1])
-// }
