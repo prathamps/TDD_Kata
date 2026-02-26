1a1d47faca47d07c90d21eac1c9731010272fb9d
prathamps <pspratham16@gmail.com>
2026-02-26 12:58:06 +0530

    test: should return sum of number itself for a comma seperated number string

--- string-calculator/src/string-calculator/string-calculator.spec.ts
+++ string-calculator/src/string-calculator/string-calculator.spec.ts
@@ -6,12 +6,21 @@ describe("StringCalculator", () => {
         expect(add("")).toBe(0);
     })
 
-    it("should return number itself for string containing single number", ()=>{
+    it("should return number itself for a single number string", ()=>{
         expect(add('1')).toBe(1)
         expect(add('2')).toBe(2)
         expect(add('3')).toBe(3)
         expect(add('21')).toBe(21)
 
     })
+
+    it('should return sum of numbers for a comma separated number string', ()=>{
+        expect(add('2,3')).toBe(5)
+        expect(add('2,3,5')).toBe(10)
+        expect(add('2,3,5,5,5')).toBe(20)
+
+    })
+
+
 
 })
--- string-calculator/src/string-calculator/string-calculator.ts
+++ string-calculator/src/string-calculator/string-calculator.ts
@@ -1,5 +1,16 @@
 export const add = (input_string: string) => {
-    if(input_string.length > 0) return Number.parseInt(input_string);
+    if(input_string.length > 0) {
+        let sum=0;
+        if(input_string.includes(",")) {
+            const num_parts = input_string.split(",")
+            num_parts.map((num)=>{
+                sum+=parseInt(num);
+            })
+            return sum;
+        }
+
+        return Number.parseInt(input_string);
+    }
 
     return 0;
 }
