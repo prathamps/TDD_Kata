3c24e48e8656012301e59b3c58c35e81f62289b0
prathamps <pspratham16@gmail.com>
2026-03-04 22:24:26 +0530

    refactore: test refactor

--- password-validator/test/password-validator-v1/password-validator-v1.test.ts
+++ password-validator/test/password-validator-v1/password-validator-v1.test.ts
@@ -1,28 +1,52 @@
-import {describe, it, expect} from "vitest"
-import { validatePassword} from "../../src/password-validator-v1/passowrd-validator";
+import { describe, it, expect } from "vitest"
+import { validatePassword } from "../../src/password-validator-v1/passowrd-validator"
 
 describe("validatePassword", () => {
-    it("should return false for empty password", () => {
-        expect(validatePassword("")).toBe(false)
+
+    it.each([
+        "",
+    ])("should return false for empty password", (password) => {
+        expect(validatePassword(password)).toBe(false)
     })
 
-    it("should return false when password has less than or equal to 8 characters", () => {
-        expect(validatePassword("password1")).toBe(false);
-    });
-
-    it("should return false when password does not contain a capital letter", () => {
-        expect(validatePassword("password1")).toBe(false)
+    it.each([
+        "Passord",
+        "abcd",
+        "12cd"
+    ])("should return false when password '%s' has less than or equal to 8 characters", (password) => {
+        expect(validatePassword(password)).toBe(false)
     })
 
-    it("should return false when password does not contain a lowercase letter", () => {
-        expect(validatePassword("PASSWORD1")).toBe(false)
+    it.each([
+        "password1",
+        "mypassword",
+        "helloworld"
+    ])("should return false when password '%s' does not contain a capital letter", (password) => {
+        expect(validatePassword(password)).toBe(false)
     })
 
-    it("should return false when password does not contain a number", () => {
-        expect(validatePassword("PASSWORDD")).toBe(false)
+    it.each([
+        "PASSWORD1",
+        "HELLO1234",
+        "WORLDTEST94"
+    ])("should return false when password '%s'does not contain a lowercase letter", (password) => {
+        expect(validatePassword(password)).toBe(false)
     })
 
-    it("should return false when password does not contain a underscore", () => {
-        expect(validatePassword("PASSWORD1")).toBe(false)
+    it.each([
+        "Password_",
+        "HelloWorld_",
+        "MySecret_"
+    ])("should return false when password '%s' does not contain a number", (password) => {
+        expect(validatePassword(password)).toBe(false)
     })
-});
+
+    it.each([
+        "Password1",
+        "HelloWorld123",
+        "WORLDTEST94"
+    ])("should return false when password '%s' does not contain an underscore", (password) => {
+        expect(validatePassword(password)).toBe(false)
+    })
+
+})
