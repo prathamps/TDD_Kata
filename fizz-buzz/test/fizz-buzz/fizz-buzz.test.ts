b3012763a33476527d886749f7e6659988a8822c
prathamps <pspratham16@gmail.com>
2026-03-05 10:22:48 +0530

    refactor: parameterized testing framework for fizzbuzz

--- fizz-buzz/test/fizz-buzz/fizz-buzz.test.ts
+++ fizz-buzz/test/fizz-buzz/fizz-buzz.test.ts
@@ -1,26 +1,35 @@
 import {describe, it, expect } from 'vitest'
 import {fizzBuzz} from '../../src/fizz-buzz/fizz-buzz'
 describe("fizzBuzz", ()=>{
-    it("returns number as a string", ()=>{
-        expect(fizzBuzz(1)).toBe("1")
-        expect(fizzBuzz(2)).toBe("2")
-        expect(fizzBuzz(4)).toBe("4")
+    it.each([
+        [1, "1"],
+        [2, "2"],
+        [4, "4"]
+    ])("returns number %d as a string '%s'", (value, expected)=>{
+        expect(fizzBuzz(value)).toBe(expected)
     })
 
-    it("returns 'fizz' for a number multiple of 3", ()=>{
-        expect(fizzBuzz(3)).toBe("fizz")
-        expect(fizzBuzz(6)).toBe("fizz")
-        expect(fizzBuzz(9)).toBe("fizz")
+    it.each([
+        [3,"fizz"],
+        [6,"fizz"],
+        [9,"fizz"],
+    ])("returns 'fizz' for a number %d multiple of 3 and not 5", (value,expected)=>{
+        expect(fizzBuzz(value)).toBe(expected)
     })
 
-    it("returns 'buzz' for a number multiple of 5", () => {
-        expect(fizzBuzz(5)).toBe("buzz")
-        expect(fizzBuzz(10)).toBe("buzz")
-        expect(fizzBuzz(20)).toBe("buzz")
+    it.each([
+        [5, "buzz"],
+        [10, "buzz"],
+        [20, "buzz"]
+    ])("returns 'buzz' for a number %d multiple of 5 and not 3", (value, expected) => {
+        expect(fizzBuzz(value)).toBe(expected)
     })
-    it("returns 'fizzBuzz' for a number multiple of both 3 and 5", () => {
-        expect(fizzBuzz(15)).toBe("fizzBuzz");
-        expect(fizzBuzz(30)).toBe("fizzBuzz");
-        expect(fizzBuzz(45)).toBe("fizzBuzz");
+
+    it.each([
+        [15,"fizzBuzz"],
+        [30,"fizzBuzz"],
+        [45,"fizzBuzz"]
+    ])("returns 'fizzBuzz' for a number %d multiple of both 3 and 5", (value,expected) => {
+        expect(fizzBuzz(value)).toBe(expected);
     })
 })
